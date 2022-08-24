bd4c010599a657282908eb846cd294a2f2a56e58
Sheila Alfaro <sa2223@njit.edu>
2022-08-24 05:21:46 -0400

    day 05, keyword throw

---
+++ day-3/day-05.js
@@ -0,0 +1,23 @@
+/*
+Author: Sheila Alfaro
+
+Complete the isPositive function below. It has one integer parameter, a. If the value of a is positive, it must return the string YES. Otherwise, 
+it must throw an Error according to the following rules:
+
+    -> If a is 0, throw an Error with  Zero Error.
+    -> If a is negative, throw an Error with message = Negative Error.
+*/
+
+function isPositive(a) {
+  try {
+    if (a > 0) {
+      return "YES";
+    } else if (a === 0) {
+      throw Error("Zero Error");
+    } else if (a < 0) {
+      throw Error("Negative Error");
+    }
+  } catch (err) {
+    return err.message;
+  }
+}
