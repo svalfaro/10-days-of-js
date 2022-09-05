5637e27963671a95681778075abcbf20e83bdfbb
Sheila Alfaro <sa2223@njit.edu>
2022-09-04 22:59:31 -0400

    day 08, regular expressions iii.

---
+++ day-6/day-08.js
@@ -0,0 +1,18 @@
+/*
+Author: Sheila Alfaro
+
+Complete the function in the editor below by returning a RegExp object, re, that matches every integer iin some string s.
+*/
+
+function regexVar() {
+  /*
+   * Declare a RegExp object variable named 're'
+   * It must match ALL occurrences of numbers in a string.
+   */
+  const re = /[0-9]+/g;
+
+  /*
+   * Do not remove the return statement
+   */
+  return re;
+}
