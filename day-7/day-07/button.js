9e678eeb05547a79adc514df0ef452891d961033
Sheila Alfaro <sa2223@njit.edu>
2022-09-05 22:52:04 -0400

    day 07, creating button.

---
+++ day-7/day-07/button.js
@@ -0,0 +1,12 @@
+// window.addEventListener('load', function() {
+//     const btn = document.querySelector("button");
+//     let current_count = 0;
+//     btn.addEventListener("click", function() {
+//         let current_count = this.innerHTML;
+//         this.innerHTML = Number(current_count)+1;
+//     });
+// })
+
+function increment() {
+  btn.innerHTML++;
+}
