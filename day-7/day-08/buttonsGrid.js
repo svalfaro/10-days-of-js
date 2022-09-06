91bc71debc51d3419ec9849c385e3f24aff505a5
Sheila Alfaro <sa2223@njit.edu>
2022-09-05 22:56:21 -0400

    day 08, button container.

---
+++ day-7/day-08/buttonsGrid.js
@@ -0,0 +1,22 @@
+const btn1 = document.querySelector("#btn1");
+const btn2 = document.querySelector("#btn2");
+const btn3 = document.querySelector("#btn3");
+const btn4 = document.querySelector("#btn4");
+const btn5 = document.querySelector("#btn5");
+const btn6 = document.querySelector("#btn6");
+const btn7 = document.querySelector("#btn7");
+const btn8 = document.querySelector("#btn8");
+const btn9 = document.querySelector("#btn9");
+// console.log(btn5);
+
+btn5.addEventListener("click", () => {
+  let count = btn1.innerHTML;
+  btn1.innerHTML = btn4.innerHTML;
+  btn4.innerHTML = btn7.innerHTML;
+  btn7.innerHTML = btn8.innerHTML;
+  btn8.innerHTML = btn9.innerHTML;
+  btn9.innerHTML = btn6.innerHTML;
+  btn6.innerHTML = btn3.innerHTML;
+  btn3.innerHTML = btn2.innerHTML;
+  btn2.innerHTML = count;
+});
